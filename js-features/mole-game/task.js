17b682fe7772b18363cac873cc8e247de7294aa3
Pavel Perebeynosov <pashketas@gmail.com>
2022-12-06 22:14:56 +0300

    Correct js-features

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,26 +1,28 @@
 const deadMoles = document.getElementById("dead");
 const misses = document.getElementById("lost");
 getHole = index => document.getElementById(`hole${index}`)
-
+let holes = [];
+function result(text){
+	alert(text);
+	deadMoles.textContent = 0;
+	misses.textContent = 0;
+}
 
 for (let i = 1; i < 10; i++) {
-	hole = getHole(i);
-	hole.addEventListener('click', function (){
-		if (hole.className.includes("hole_has-mole")) {
-		deadMoles.textContent = Number(deadMoles.textContent) + 1;
-			if (deadMoles.textContent == 10) {
-				alert("Поздравляю! Вы победили!");
-				deadMoles.textContent = 0;
-				misses.textContent = 0;
-			}
-		} else {
-		misses.textContent = Number(deadMoles.textContent) + 1;
-			if (misses.textContent == 5) {
-				alert("Вы проиграли! Удачи в следующий раз");
-				deadMoles.textContent = 0;
-				misses.textContent = 0;
-			}
-		}
-	});
+	holes.push(getHole(i));
 }
+
+holes.forEach(item => item.addEventListener('click', function (){
+	if (item.className.includes("hole_has-mole")) {
+		deadMoles.textContent = Number(deadMoles.textContent) + 1;
+	} else {
+		misses.textContent = Number(deadMoles.textContent) + 1;
+	}
+	if (Number(deadMoles.textContent) >= 10) {
+		result("Поздравляю! Вы победили!");
+	} else if (Number(misses.textContent) >= 5) {
+		result("Вы проиграли! Удачи в следующий раз");
+	}
+}));
+
 
